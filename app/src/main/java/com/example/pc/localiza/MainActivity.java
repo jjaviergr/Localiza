f44fe63a17ea0165a9e5bbc65b49cc9e8fa0bcac
jjaviergr1176 <jjaviergr1176@gmail.com>
2016-01-26 16:19:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pc/localiza/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.pc.localiza;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?z=4&q=restaurantes"));
+        startActivity(intent);
+
+    }
+}
